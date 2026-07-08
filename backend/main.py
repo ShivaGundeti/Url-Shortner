68c26e67df9d6013cd7ae7aa2163d82c1bb9e137
Shiva Gundeti <shivagundeti1403@gmail.com>
2026-07-08 16:18:49 +0530

    feat: implement JWT authentication and secure routes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from ipaddress import ip_address
 from fastapi import HTTPException, FastAPI, Request
 from fastapi import FastAPI
 from prisma import Prisma
@@ -8,7 +7,15 @@ import random
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 import redis
+from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timedelta
+from fastapi import Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 app = FastAPI()
+security = HTTPBearer()
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"], # This tells Python that Next.js is allowed!
@@ -16,11 +23,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+SECRET_KEY = "exwehyuioppanjzz"
 db = Prisma()
 cache = redis.Redis(host="url-redis", port=6379, db=0, decode_responses=True)
 class URLRequest(BaseModel):
     original_url: str
+
+class PassRequest(BaseModel):
+    password: str
+
+class AuthRequest(BaseModel):
+    email: str
+    password: str
 
 
 @app.on_event("startup")
@@ -35,8 +49,64 @@ async def shutdown():
 async def root():
     return {"Message":"Welcome to the Url shortner API!!"}
 
+
+@app.post("/url/test-api")
+async def TestPass(url: PassRequest):
+    password = url.password
+    hashed = pwd_context.hash(password)
+    return {"Sucess": hashed}
+
+@app.post("/url/register")
+async def SignUp(request: AuthRequest):
+    user_email = request.email
+    user_password = request.password
+    if not user_email.strip() or not user_password.strip():
+        raise HTTPException(status_code=400,detail="Email and Password are required")
+    else:
+        hashed_password = pwd_context.hash(user_password)
+        Save_User = await db.user.create(
+            data = {
+             "email":user_email,"password":hashed_password
+            }
+        )
+        return {"Status:":201,"Sucess: ":Save_User}
+
+
+@app.post("/url/login")
+async def SignIn(request: AuthRequest):
+    user_email = request.email
+    user_password = request.password
+    if not user_email.strip() or not user_password.strip():
+        raise HTTPException(status_code=400,detail="Email and password are required")
+    else:
+        
+        User = await db.user.find_first(
+            where={
+                "email":user_email
+            }
+        )
+        if not User:
+            raise HTTPException(status_code=404,detail="Incorrect email or password")
+        is_valid = pwd_context.verify(user_password, User.password)
+    
+        if not is_valid:
+            raise HTTPException(status_code=401, detail="Incorrect email or password")
+        expire = datetime.utcnow() + timedelta(minutes=60)
+        payload = {"sub":User.id,"exp":expire}
+        token = jwt.encode(payload,SECRET_KEY,algorithm="HS256")
+        return {"access_token": token, "token_type": "bearer"}
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Security(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload.get("sub") # Returns the User ID!
+    except:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+
 @app.post("/url/shorten")
-async def UrlShortner(url: URLRequest,request: Request):
+async def UrlShortner(url: URLRequest, request: Request, user_id: str = Security(verify_token)):
     ip_address = request.client.host
     redis_key = f"rate_limit:post:{ip_address}"
     print("ip_address: ",ip_address)
@@ -49,7 +119,7 @@ async def UrlShortner(url: URLRequest,request: Request):
     originalUrl = url.original_url
     randomStr = "".join(random.choices(string.ascii_letters,k=5))
     ShortUrl = await db.url.create(
-        data={"original_url": originalUrl, "short_url": randomStr}
+        data={"original_url": originalUrl, "short_url": randomStr,"userId":user_id}
     )
     return{"status": "Success","data": ShortUrl}
 
